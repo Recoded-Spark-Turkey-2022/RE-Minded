b5538e0f5ca6e25986605a2b82bd7dc33724dcb8
HomeLanderz <86560837+HomeLanderz@users.noreply.github.com>
2022-12-26 13:58:43 +0300

    Style: add the google font Poppins to the html file, use it in the component

--- src/ComponentFolders/LoginPage/Login.jsx
+++ src/ComponentFolders/LoginPage/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
         <div className='flex justify-center my-10'>
 
             <div>
-            <h2 className='text-5xl font-poppins font-normal mb-28'>LOGIN</h2>
+            <h2 className='text-5xl font-["Poppins"] font-normal mb-28'>LOGIN</h2>
             <form className='grid grid-rows-3 gap-12 shadow-2xl px-10 py-10'>
                 <input type="text" placeholder='   Your Email' className='h-20 broder-solid border-2 border-[#D1DBE3] rounded-md'/>
                 <input type="text" placeholder='   Your Password' className='h-20 broder-solid border-2 border-[#D1DBE3] rounded-md'/>
