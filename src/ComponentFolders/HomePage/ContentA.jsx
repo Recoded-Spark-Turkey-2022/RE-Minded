6ca11d2ca8c33b00ea069f37693b6565857c7053
Menar Selamet <menarselamet007@gmail.com>
2022-12-26 21:35:16 +0300

    Design: Positioning images

--- src/ComponentFolders/HomePage/ContentA.jsx
+++ src/ComponentFolders/HomePage/ContentA.jsx
@@ -5,9 +5,15 @@ import Sofa from './Images/ContentA(sofa).svg';
 function ContentA () {
     return (
         <>
-        <img src={Background} alt='Background'/>
-        <img src={Sofa} alt='Sofa'/>
-        <button type="button"> BOOK AN APPOINTMENT </button>
+        <img className="absolute inset-0" src={Background} alt='Background'/> 
+        <div className="relative flex justify-between">
+        <div>
+        <p> WE ARE HERE TO</p>
+        <p>HELP</p> 
+        <button type="button" className=" rounded-md box-border p-3 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 "> BOOK AN APPOINTMENT </button>
+        </div>
+        <img className="h-2/5 w-2/5" src={Sofa} alt='Sofa'/>
+        </div>
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+       backgroundImage: {
+        'Background': "url('/src/ComponentsFolders/HomePage/Images/Background.svg')",
+  
+      }
+    },
   },
   plugins: [],
 };
