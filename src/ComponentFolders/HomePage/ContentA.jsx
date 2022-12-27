2a74b2436dbc32754e169c326473862173d451af
Menar Selamet <menarselamet007@gmail.com>
2022-12-27 11:56:44 +0300

    Design: image positioning

--- src/ComponentFolders/HomePage/ContentA.jsx
+++ src/ComponentFolders/HomePage/ContentA.jsx
@@ -6,13 +6,15 @@ function ContentA () {
     return (
         <>
         <img className="absolute inset-0" src={Background} alt='Background'/> 
-        <div className="relative flex justify-between">
-        <div>
-        <p> WE ARE HERE TO</p>
-        <p>HELP</p> 
-        <button type="button" className=" rounded-md box-border p-3 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 "> BOOK AN APPOINTMENT </button>
+        <div className="relative flex lg:justify-between lg:m-56">
+        <div className=" flex flex-col">
+        <div className="pt-30">
+        <p className="lg:text-3xl  pl-5"> WE ARE HERE TO</p>
+        <p className="lg:text-8xl  pl-5">HELP</p> 
         </div>
-        <img className="h-2/5 w-2/5" src={Sofa} alt='Sofa'/>
+        <button type="button" className="rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 "> BOOK AN APPOINTMENT </button>
+        </div>
+        <img className="h-3/5 w-3/5" src={Sofa} alt='Sofa'/>
         </div>
         </>
     )
