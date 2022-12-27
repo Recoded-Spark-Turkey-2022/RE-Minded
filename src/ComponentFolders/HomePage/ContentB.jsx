30779ea91f10577eb6c1f26252d51111ece86903
Menar Selamet <menarselamet007@gmail.com>
2022-12-27 19:45:51 +0300

    Build: Style modifications

--- src/ComponentFolders/HomePage/ContentB.jsx
+++ src/ComponentFolders/HomePage/ContentB.jsx
@@ -5,11 +5,11 @@ function ContentB() {
   return (
     <div>
       <div className="bg-amber-200">
-        <h1 className="">
+        <h1 className="lg:text-5xl text-lg font-bold lg:w-7/12 lg:font-normal lg:pt-20 pt-20 pl-16">
           PROFESSIONAL, LICENSED, AND VETTED THERAPISTS THAT YOU CAN TRUST
         </h1>
-        <img src={Therapists} alt="therapists" />
-        <p>
+        <img className="pt-16 pb-16 pl-16" src={Therapists} alt="therapists" />
+        <p className="pl-16 lg:text-lg text-sm lg:w-8/12 lg:pb-20">
           Tap into the worlds largest network of licensed, accredited, and
           experienced therapists who can help you with a range of issues
           including depression, anxiety, relationships, trauma, grief, and more.
@@ -19,7 +19,7 @@ function ContentB() {
         </p>
         <button
           type="button"
-          className="rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 "
+          className=" ml-16 lg:px-6 rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 "
         >
           BOOK AN APPOINTMENT
         </button>
