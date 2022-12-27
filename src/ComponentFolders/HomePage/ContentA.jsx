4205858f643cf7f03a42199e839b9420c88b6d26
Menar Selamet <menarselamet007@gmail.com>
2022-12-27 14:31:30 +0300

    Design: position changing

--- src/ComponentFolders/HomePage/ContentA.jsx
+++ src/ComponentFolders/HomePage/ContentA.jsx
@@ -1,23 +1,31 @@
-import React from "react";
+import React from 'react';
 import Background from './Images/Background.svg';
 import Sofa from './Images/ContentA(sofa).svg';
+import './main.css';
 
-function ContentA () {
-    return (
-        <>
-        <img className="absolute inset-0" src={Background} alt='Background'/> 
-        <div className="relative flex lg:justify-between lg:m-56">
-        <div className=" flex flex-col">
-        <div className="pt-30">
-        <p className="lg:text-3xl  pl-5"> WE ARE HERE TO</p>
-        <p className="lg:text-8xl  pl-5">HELP</p> 
+function ContentA() {
+  return (
+    <>
+      <img className="absolute inset-0" src={Background} alt="Background" />
+      <div className="">
+        <div className="relative flex justify-between m-44 bottom-5">
+          <div>
+            <p className="text-3xl"> WE ARE HERE TO</p>
+            <p className="text-8xl">HELP</p>
+            <button
+              type="button"
+              className="rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 "
+            >
+              BOOK AN APPOINTMENT
+            </button>
+          </div>
+          <div className=" absolute h-4/6 w-4/6">
+            <img className="relative left-72 bottom-16" src={Sofa} alt="Sofa" />
+          </div>
         </div>
-        <button type="button" className="rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 "> BOOK AN APPOINTMENT </button>
-        </div>
-        <img className="h-3/5 w-3/5" src={Sofa} alt='Sofa'/>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default ContentA;
