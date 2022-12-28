34ea53a50056b49e81a01896de7fdbf8ac21a70f
Menar Selamet <menarselamet007@gmail.com>
2022-12-28 14:17:47 +0300

    Build: Page layout

---
+++ src/ComponentFolders/HomePage/ContentE.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function ContentE() {
+  return (
+    <>
+      <h1>PURCHASE TICKETS</h1>
+      <h3>PURCHASE TICKETS THAT CAN BE USED TO BOOK APPOINTMENTS!</h3>
+      <div>tickets</div>
+      <div className="flex flex-col justify-around items-center p-4 rounded-lg">
+        <h1>ARE YOU A COUNSELOR?</h1>
+        <p>
+          Interested in joining our mental health platform? You decide your
+          schedule and how much you want to work, well take care of the client
+          referrals and billing details!
+        </p>
+        <button type="button"> LEARN MORE </button>
+      </div>
+    </>
+  );
+}
+
+export default ContentE;
