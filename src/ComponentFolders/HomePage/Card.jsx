494884c8d077ffdae983bbcd7b26f90986be2a11
Menar Selamet <menarselamet007@gmail.com>
2022-12-29 20:16:05 +0300

    Final

--- src/ComponentFolders/HomePage/Card.jsx
+++ src/ComponentFolders/HomePage/Card.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 function Card({ image, title, content, price, ticket, button, buttonPreview }) {
   return (
     <div className="shadow-zinc-300 space-x-4 box-border h-96 w-80  my-4 flex flex-col items-center justify-evenly shadow-md  rounded-3xl md:w-1/4 p-4">
-      <img className="p-4" src={image} alt={title} />
+      <img className="p-8 " src={image} alt={title} />
       <h1 className=" text-center text-xl lg:text-3xl 2xl:text-4xl pb-4">
         {title}
       </h1>
-      <p className="text-xl flex justify-center pb-8 text-gray-500">
+      <p className="lg:text-xl p-2 md:text-sm flex justify-center pb-8 text-gray-500">
         {content}
       </p>
       <h2 className="text-3xl pb-4">{ticket}</h2>
