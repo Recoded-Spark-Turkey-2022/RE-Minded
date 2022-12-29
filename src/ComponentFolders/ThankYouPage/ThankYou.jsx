48d71514536260485a0335aed9b916f8093e7a71
Selvi Ece Dugan <selvidugan@gmail.com>
2022-12-29 15:17:35 +0300

    responsive design added

--- src/ComponentFolders/ThankYouPage/ThankYou.jsx
+++ src/ComponentFolders/ThankYouPage/ThankYou.jsx
@@ -2,18 +2,23 @@ import React from 'react'
 
 const ThankYou = () => {
   return (
-    <div>
-        <h1 className='absolute w-[300px] h-[75px] top-[156px] left-[84px] font-poppins text-5xl text-black'>
-            THANK YOU!
-        </h1>
-        <div>
-            <p className='absolute w-[1273px] h-[36px] top-[251px] left-[84px] font-poppins text-2xl leading-9 text-SubTexts'>
-                Your email has been added to the mailing list successfully!
-            </p>
-        </div>
-        <div>
-          <button type= "button" className='bg-Buttons absolute w-[300px] h-[60px] top-[343px] left-[84px] rounded-md font-poppins text-2xl font-normal leading-9 tracking-normal text-center '> BACK TO HOME</button>
-        </div>
+    <div className='flex flex-col w-screen h-screen font-poppins text-black tracking-[-0.01em]'>
+
+      <h1 className='absolute flex flex-wrap w-[300px] h-[75px] top-[156px] left-[84px]  text-3xl lg:text-5xl '>
+        THANK YOU!
+      </h1>
+      <div>
+        <p className='absolute flex flex-wrap w-[1273px] h-[36px] top-[251px] left-[84px] sm:text-lg leading-6 text-2xl lg:text-3xl leading-9 text-SubTexts'>
+          Your email has been added to the mailing list successfully!
+        </p>
+      </div>
+
+      <div>
+        <button type="button" className='bg-Buttons absolute w-[200px] md:[250px] lg:w-[300px] h-[60px] top-[343px] left-[84px]  rounded-md font-poppins text-2xl font-normal leading-9 tracking-normal text-center '>
+          BACK TO HOME
+        </button>
+      </div>
+
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,22 +8,20 @@ module.exports = {
       },
     },
     colors: {
-      Background:'#E5E5E5',
+      Background: '#E5E5E5',
       Navbar: '#EAF8F9',
       Footer: '#FEE89E',
       Buttons: '#2DD3E3',
       BlackTexts: '#1A1A1A',
-      SubTexts:'#718096'
+      SubTexts: '#718096'
     },
     screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
-    
+
   },
   plugins: [],
 };
