64c9d8523c3810aed35fae3fa6fd06642048c5ca
SeymaDemir <seymademir35@gmail.com>
2022-12-29 23:19:54 +0300

    fixed style

--- src/ComponentFolders/AboutPage/About.jsx
+++ src/ComponentFolders/AboutPage/About.jsx
@@ -19,7 +19,7 @@ function About() {
           for everyone through our software, education, and community.
         </div>
       </div>
-      <div className="flex flex-grow flex-col lg:flex-row bg-cyan-50 w-full ">
+      <div className="flex items-center flex-grow flex-col lg:flex-row bg-cyan-50 w-full ">
         <div className="self-center lg:ml-[10%] lg:mt-[0%] mt-[2%]">
           <img
             src={aboutImg}
@@ -27,7 +27,7 @@ function About() {
             className=" sm:w-[180px] sm:h-[180px] md:w-[220px] md:h-[220px] lg:w-[65em] lg:h-[15em] rounded-md object-cover"
           />
         </div>
-        <div className="text-gray-700 ml-[10%] lg:ml-[5%] mr-[8%] mt-[5%] lg:mb-[0%] mb-[5%] ">
+        <div className="text-gray-700 ml-[10%] lg:ml-[5%] mr-[8%] lg:mt-[0%] mt-[5%] lg:mb-[0%] mb-[5%] ">
           <h2 className="lg:text-4xl text-2xl ">Our Founding</h2>
           <div className="md:text-lg text-sm lg:mt-[2%] mt-[4%]">
             Healing was founded by Ammar Sohail in 2021. It was called Online
