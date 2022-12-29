9541fb0c74ca4dd21d43f0089b756565d5383355
Menar Selamet <menarselamet007@gmail.com>
2022-12-29 11:12:49 +0300

    Build: Tickets added

--- src/ComponentFolders/HomePage/Card.jsx
+++ src/ComponentFolders/HomePage/Card.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-function Card({ image, title, content }) {
+function Card({ image, title, content, price, ticket, button }) {
   return (
     <div>
-        <h1>{title}</h1>
-        <img src={image} alt={title}/>
-        <p>{content}</p>
+      <h1>{title}</h1>
+      <img src={image} alt={title} />
+      <p>{content}</p>
+      <h2>{ticket}</h2>
+      <h2>{price}</h2>
+      <button
+        type="button"
+        className="rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500"
+      >
+        {button}
+      </button>
     </div>
   );
 }
--- src/ComponentFolders/HomePage/ContentE.jsx
+++ src/ComponentFolders/HomePage/ContentE.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Card from './Card';
 
 function ContentE() {
   return (
     <>
       <h1>PURCHASE TICKETS</h1>
-      <h3>PURCHASE TICKETS THAT CAN BE USED TO BOOK APPOINTMENTS!</h3>
-      <div>tickets</div>
+      <h4>PURCHASE TICKETS THAT CAN BE USED TO BOOK APPOINTMENTS!</h4>
+      <div className='flex flex-row justify-center p-4 m-4'>
+        
+        <Card ticket="5 TICKETS" price="10$" button="PURCHASE" />
+
+        <Card ticket="25 TICKETS" price="40$" button="PURCHASE" />
+
+        <Card ticket="50 TICKETS" price="70$" button="PURCHASE" />
+      </div>
       <div className="flex flex-col justify-around items-center p-4 rounded-lgshadow-2xl px-10 py-10 h-20 broder-solid border-2 border-[#D1DBE3] rounded-md">
         <h1>ARE YOU A COUNSELOR?</h1>
         <p>
@@ -15,7 +23,8 @@ function ContentE() {
         </p>
         <button
           type="button"
-          className="rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500">
+          className="rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500"
+        >
           LEARN MORE
         </button>
       </div>
