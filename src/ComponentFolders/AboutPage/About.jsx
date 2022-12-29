d012bf7ca6743a343286d1cc78f1bcbf3d2340e7
SeymaDemir <seymademir35@gmail.com>
2022-12-29 12:32:47 +0300

    Fixed styles

--- src/ComponentFolders/AboutPage/About.jsx
+++ src/ComponentFolders/AboutPage/About.jsx
@@ -5,9 +5,9 @@ function About() {
   return (
     <div className="flex flex-col w-screen h-screen font-poppins text-black tracking-[-0.01em]">
       <div className="flex flex-grow bg-white flex-col lg:mb-[0%] mb-[5%] ml-[10%] mt-[5%] mr-[8%]">
-        <h2 className="lg:text-5xl text-3xl">HEALING!</h2>
+        <h2 className="lg:text-5xl text-3xl">HEALING</h2>
         <h3 className="lg:text-3xl text-xl text-black text-opacity-50">
-          some cool liner !
+          We are here to HELP!
         </h3>
         <div className="md:text-lg text-sm leading-6 mt-[5%]">
           At Healing, we believe there is a better way to do things. A more
@@ -19,7 +19,7 @@ function About() {
           for everyone through our software, education, and community.
         </div>
       </div>
-      <div className="flex flex-grow flex-col lg:flex-row bg-blue-100 w-full ">
+      <div className="flex flex-grow flex-col lg:flex-row bg-cyan-50 w-full ">
         <div className="self-center lg:ml-[10%] lg:mt-[0%] mt-[2%]">
           <img
             src={aboutImg}
@@ -30,12 +30,12 @@ function About() {
         <div className="text-gray-700 ml-[10%] lg:ml-[5%] mr-[8%] mt-[5%] lg:mb-[0%] mb-[5%] ">
           <h2 className="lg:text-4xl text-2xl ">Our Founding</h2>
           <div className="md:text-lg text-sm lg:mt-[2%] mt-[4%]">
-            Healing was founded by Ammar Shoail in 2021. It was called Healing
-            Online, and started as a blog and an online community where some of
-            the world&apos;s therapists shared their research and ideas. We
+            Healing was founded by Ammar Sohail in 2021. It was called Online
+            Therapist, and started as a blog and an online community where some
+            of the world&apos;s therapists shared their research and ideas. We
             launched the Beginner&apos;s Guide to Therapy and our first study,
             and that hub of industry expertise transformed into a small
-            consulting firm and led us to create the Online Therapist of today!.
+            consulting firm and led us to create the Online Therapist of today!
           </div>
         </div>
       </div>
