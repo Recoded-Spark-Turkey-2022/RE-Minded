c9cb87c4bc1a3ccd50922fd5069dae3439939d46
selvidugan <selvidugan@gmail.com>
2022-12-29 23:14:04 +0300

    design: some responsiveness efforts

--- src/ComponentFolders/Footer/Footer.jsx
+++ src/ComponentFolders/Footer/Footer.jsx
@@ -7,27 +7,36 @@ import image4 from './Images/SubscribeArrow.svg'
 
 function Footer() {
   return (
-    <div className='grid-cols-3 gap-8 w-full h-[240px] top-[2832px] left-0 bottom-0 rounded-none absolute flex-wrap bg-Footer'>
+    <div className='grid-cols-3 gap-8 xs:w-full h-[240px] top-[2832px] left-0 bottom-0 rounded-none absolute flex-wrap bg-Footer'>
       <div className='grid-cols-1 p-o gap-6 absolute w-[500px] h-[160px] left-[87px] top-[40px] rounded-none'>
         <div className='items-center flex-col p-o gap-3 w-[500px] h-[78px]'>
-          <h1 className='text-4xl font-medium leading-[44xp] tracking-[0.18px] w-[500px] h-[44px] text-BlackTexts'>Subscribe</h1>
-          <p className='text-SubTexts w-[500px] h-[22px] top-[56px] font-normal text-xl leading-5 tracking-[0.075px]'>We’ll never spam to you or share your email</p>
+          <h1 className='text-4xl font-medium leading-[44xp] tracking-[0.18px] w-[500px] h-[44px] text-BlackTexts'>
+            Subscribe
+          </h1>
+          <p className='text-SubTexts w-[500px] h-[22px] top-[56px] font-normal text-xl leading-5 tracking-[0.075px]'>
+            We’ll never spam to you or share your email
+          </p>
         </div>
-        <div>
+        
+        <div className='md:flex'>
+          <div className='md:shrink-0'>
           <div className='flex flex-wrap mb-4 w-[300px] h-[50px] box-border rounded-md border-2 border-[#718096]'>
             <input className=' w-3/4 h-auto rounded-l-lg text-SubTexts font-normal ' type="email" placeholder='  Enter your e-mail' />
-            <button type="button" className='bg-Buttons rounded-r-lg w-1/4 h-auto '>
-              <img className='w-[24px] h-[24px] top-[18px] left-[310px] items-center mx-auto' src={image4} alt='Arrow' />
-            </button>
-
+                <button type="button" className='bg-Buttons rounded-r-lg w-1/4 h-auto '>
+                    <img className='w-[24px] h-[24px] top-[18px] left-[310px] items-center mx-auto' src={image4} alt='Arrow' />
+                </button>
+          </div>
           </div>
         </div>
       </div>
-      <div>
-        <div className='grid grid-cols-4 p-o gap-6 absolute w-[500px] h-[160px] right-[87px] top-[65px] text-SubTexts font-normal text-xl leading-5 tracking-[0.075px]'>
+      
+      
+        <div className='grid grid-cols-4 gap-6 absolute  
+              w-[33%] h-[160px] right-[87px] top-[65px]
+             text-SubTexts font-normal text-xl leading-5 tracking-[0.075px]'>
           <ul>
             <li>Home</li>
-          </ul>
+          </ul> 
           <ul>
             <li>Blogs</li>
           </ul>
@@ -37,6 +46,7 @@ function Footer() {
           <ul>
             <li>Contact</li>
           </ul>
+
           <div className='justify-center mx-36 w-[200px] h-[78px]'>
             <div className='flex flex-row justify-center mx-8 space-x-10 '>
               <img src={image3} alt='Twitter Logo' />
@@ -46,7 +56,7 @@ function Footer() {
             </div>
           </div>
         </div>
-      </div>
+      
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,27 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
     colors: {
-      Background:'#E5E5E5',
+      Background: '#E5E5E5',
       Navbar: '#EAF8F9',
-      Footer: '#FEE89E',
+      Footer: '#FEE89E',    
       Buttons: '#2DD3E3',
       BlackTexts: '#1A1A1A',
-      SubTexts:'#718096'
-    }
+      SubTexts: '#718096'
+    },
+    screens: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+
   },
   plugins: [],
 };
