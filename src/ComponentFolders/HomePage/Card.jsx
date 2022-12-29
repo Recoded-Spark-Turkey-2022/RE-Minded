07a8ff8988c68ecf73de16968959a5feee092a8d
Menar Selamet <menarselamet007@gmail.com>
2022-12-29 14:28:39 +0300

    Responsiveness check

--- src/ComponentFolders/HomePage/Card.jsx
+++ src/ComponentFolders/HomePage/Card.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 function Card({ image, title, content, price, ticket, button }) {
   return (
-    <div className="font-poppins h-full w-full my-4 flex flex-col items-center justify-evenly shadow-lg rounded-lg md:w-1/4 p-4">
-      <h1 className='text-center text-xl lg:text-3xl 2xl:text-title pb-1'>{title}</h1>
+    <div className="shadow-zinc-300 font-poppins h-full w-full my-4 flex flex-col items-center justify-evenly shadow-lg rounded-lg md:w-1/4 p-4">
+      <h1 className="text-center text-xl lg:text-3xl 2xl:text-title pb-4">
+        {title}
+      </h1>
       <img src={image} alt={title} />
       <p>{content}</p>
-      <h2 className="text-3xl">{ticket}</h2>
-      <h2 className="text-2xl flex justify-center">{price}</h2>
+      <h2 className="text-3xl pb-4">{ticket}</h2>
+      <h2 className="text-2xl flex justify-center pb-4">{price}</h2>
       <button
         type="button"
         className="font-poppins rounded-md box-border p-2 m-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500"
--- src/ComponentFolders/HomePage/ContentE.jsx
+++ src/ComponentFolders/HomePage/ContentE.jsx
@@ -10,18 +10,18 @@ function ContentE() {
       <h4 className="pl-16 font-poppins text-xl pb-8 ">
         PURCHASE TICKETS THAT CAN BE USED TO BOOK APPOINTMENTS!
       </h4>
-      <div className="flex flex-col md:flex-row items-center justify-around w-full lg:h-1/3">
+      <div className="flex flex-col md:flex-row items-center justify-around w-full lg:h-1/3 pb-4">
         <Card ticket="5 TICKETS" price="10$" button="PURCHASE" />
 
         <Card ticket="25 TICKETS" price="40$" button="PURCHASE" />
 
         <Card ticket="50 TICKETS" price="70$" button="PURCHASE" />
       </div>
-      <div className=" font-poppins flex flex-col justify-around items-center p-4 rounded-lg h-1/3 lg:mx-16 my-4  shadow-lg ">
+      <div className=" shadow-zinc-300 font-poppins flex flex-col justify-around items-center p-4 rounded-lg h-1/3 lg:mx-16 my-4  shadow-lg ">
         <h1 className="text-2xl lg:text-3xl uppercase text-center py-4">
           ARE YOU A COUNSELOR?
         </h1>
-        <p className="font-poppins w-2/4  md:text-md  text-justify md:text-center text-opacity-50">
+        <p className="font-poppins text-xl w-2/4  md:text-md  text-justify md:text-center text-opacity-50">
           Interested in joining our mental health platform? You decide your
           schedule and how much you want to work, well take care of the client
           referrals and billing details!
