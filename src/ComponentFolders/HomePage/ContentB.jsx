5d85319e76729e55298529cad7eb47669346964f
Menar Selamet <menarselamet007@gmail.com>
2022-12-29 20:02:38 +0300

    Final

--- src/ComponentFolders/HomePage/ContentB.jsx
+++ src/ComponentFolders/HomePage/ContentB.jsx
@@ -5,11 +5,11 @@ function ContentB() {
   return (
     <div>
       <div className="bg-amber-200 pb-16 font-poppins">
-        <h1 className="lg:text-5xl text-lg font-bold lg:w-7/12 lg:font-normal lg:pt-20 pt-20 pl-16">
+        <h1 className="tracking-wider lg:text-5xl text-lg font-bold lg:w-7/12 lg:font-poppins lg:pt-20 pt-20 pl-16 sm:p-16 sm:font-poppins">
           PROFESSIONAL, LICENSED, AND VETTED THERAPISTS THAT YOU CAN TRUST
         </h1>
         <img className="pt-16 pb-16 pl-16" src={Therapists} alt="therapists" />
-        <p className="pl-16 lg:text-xl text-sm lg:w-8/12 lg:pb-20">
+        <p className="pl-16 lg:text-2xl font-poppins text-sm lg:w-8/12 lg:pb-20 sm:pb-16">
           Tap into the worlds largest network of licensed, accredited, and
           experienced therapists who can help you with a range of issues
           including depression, anxiety, relationships, trauma, grief, and more.
@@ -17,9 +17,10 @@ function ContentB() {
           would expect from an in-office therapist, but with the ability to
           communicate whenever and however you want.
         </p>
+
         <button
           type="button"
-          className=" ml-16 lg:px-6 rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 "
+          className="text-xl md:text-lg ml-16 lg:px-6 rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 "
         >
           BOOK AN APPOINTMENT
         </button>
