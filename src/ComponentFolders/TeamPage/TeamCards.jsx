d9b181cc65d3b0aeecebbb8e5d97aeded464a672
SeymaDemir <seymademir35@gmail.com>
2022-12-30 12:09:05 +0300

    continue for styling

--- src/ComponentFolders/TeamPage/TeamCards.jsx
+++ src/ComponentFolders/TeamPage/TeamCards.jsx
@@ -3,22 +3,19 @@ import React from 'react'
 
 function TeamCards({name, job, photo}) {
   return (
-    <div className="w-full rounded-lg shadow-md lg:max-w-sm p-[1%]">
+    <div className="w-[20em] bg-[#EAF8F9] rounded-[2em] shadow-md lg:max-w-sm p-[1%]">
     <img
-        className="object-cover object-center w-full h-96"
+        className="object-cover object-center w-full h-96 rounded-t-[2em]"
         src={photo}
         alt="seyma"
     />
-    <div className="p-4">
-        <h4 className="text-xl font-semibold tracking-tight text-blue-600">
+    <div className="p-4 flex flex-col items-center">
+        <h4 className="text-2xl font-semibold tracking-tight text-[#424A4F] ">
             {name}
         </h4>
-        <p className="mb-2 leading-normal">
+        <p className="text-lg mb-2 leading-normal">
             {job}
         </p>
-        {/* <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-            Read more
-        </button> */}
     </div>
 </div>
   )
--- src/ComponentFolders/TeamPage/TeamPageMain.jsx
+++ src/ComponentFolders/TeamPage/TeamPageMain.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import TeamCards from './TeamCards'
 import menar from './Images/Menar Selamet_Turkey_2022.jpg'
-import sohail  from './Images/Sohail Ahmed_Turkey_2022.jpeg'
-import seyma from './Images/Seyma Demir_Turkey_2022.jpeg'
+import sohail  from './Images/SohailAhmed.jpeg'
+import seyma from './Images/Seyma Demir_Turkey_2022.jpg'
 import selvi from './Images/Selvi Ece Dugan_Turkey_2022.jpg'
 import ammar from './Images/Ammar Eldik_Turkey_2022.jpg'
 
 
 // name and jobs for pass data to card with props
 // do we need to show team trainer?
-const names = ["Menar Selamet", "Sohail Ahmed", "Seyma Demir", "Selvi Ece Dugan", "Ammar Eldik"]
+const names = ["Menar Selamet", "Sohail Ahmed", "Şeyma Demir", "Selvi Ece Dugan", "Ammar Eldik"]
 const jobs = ["Lead Engineer", "Frontend Developer"]
 const photos = [menar, sohail, seyma, selvi, ammar]
 
 function TeamPageMain() {
   return (
-    <div className='flex flex-col'>
-        <div className='font-poppins flex-grow-2'>WE ARE HEALING, NICE TO MEET YOU!</div>
-        <div className='font-poppins flex felx-rows flex-grows'>
+    <div className='flex flex-col text-[#424A4F]'>
+        <div className='font-poppins flex-grow-2 text-5xl leading-10'>WE ARE HEALING, NICE TO MEET YOU!</div>
+        <div className='text-3xl leading-10'>Meet the Team!</div>
+        <div className='font-poppins flex flex-rows justify-items-center justify-between'>
             {names.map((name, index) => {
                 return <TeamCards name={name} job={jobs[1]} photo={photos[index]} />
             })}
