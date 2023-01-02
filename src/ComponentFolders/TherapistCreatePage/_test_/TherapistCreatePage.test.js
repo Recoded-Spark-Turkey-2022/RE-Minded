2572415bc2b41b61f62057e050688e251ba4542b
SeymaDemir <seymademir35@gmail.com>
2023-01-02 10:55:31 +0300

    completed without Link

--- src/ComponentFolders/TherapistCreatePage/_test_/TherapistCreatePage.test.js
+++ src/ComponentFolders/TherapistCreatePage/_test_/TherapistCreatePage.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import TherapistCreatePage from '../TherapistCreatePage';
 
-test('About Page snapshot test', () => {
+test('Therapist Page snapshot test', () => {
   const component = renderer.create(<TherapistCreatePage />);
 
   const tree = component.toJSON();
