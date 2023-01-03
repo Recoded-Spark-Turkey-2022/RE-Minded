63b2e15ec66f37afeb6df897832043e694543c60
SeymaDemir <seymademir35@gmail.com>
2023-01-03 12:20:28 +0300

    need fixing responsive design

--- src/ComponentFolders/EditProfilePage/EditProfileMain.jsx
+++ src/ComponentFolders/EditProfilePage/EditProfileMain.jsx
@@ -1,38 +1,41 @@
 import React from 'react';
 import profilePhoto from './Images/ProfilePhoto.svg';
 import profileIcon from './Images/profileIcon.svg'
+import plusIcon from './Images/PlusIcon.svg'
+import passwordIcon from './Images/PasswordIcon.svg'
+// import arrowIcon from './Images/ArrowIcon.svg'
 
 function EditProfileMain() {
   return (
-    <div className="flex flex-col font-poppins items-center">
-      <div className="self-center mt-8 text-xl text-[#FF0000]">
+    <div className="flex flex-col font-poppins lg:items-center lg:pr-0 pr-10">
+      <div className="self-center mt-8 lg:text-xl text-sm text-[#FF0000] lg:ml-0 ml-16 lg:mr-0 mr-[-1em]">
         Please fill all the fields with correct and valid details to complete
         your profile.
       </div>
-      <div className="flex flex-row">
-        <div className='flex flex-col ml-[-10em]'>
+      <div className="flex lg:flex-row flex-col">
+        <div className='flex flex-col lg:ml-[-10em] md:ml-[10%] ml-[25%] lg:mr-[0%] md:mr-[30%] mr-[25%]'>
           <img src={profilePhoto} alt="profile" className="self-center ml-28" />
-          <img src={profileIcon} alt="profileIcon" className='w-16 ml-32 self-center mt-[-4em]'/>
+          <img src={profileIcon} alt="profileIcon" className='lg:w-16 w-14 ml-32 self-center lg:mt-[-4em] mt-[-3em]'/>
         </div>
-        <div className="flex flex-col ml-16">
-          <div className=" ml-20 self-start mr-44 mt-6 ">
-            <h1 className="text-5xl">PROFILE INFO</h1>
+        <div className="flex flex-col lg:ml-16 ml-44">
+          <div className=" lg:ml-20 ml-[-15em] lg:self-start lg:mr-44 mt-6 ">
+            <h1 className="lg:text-5xl text-2xl lg:ml-0 ml-20">PROFILE INFO</h1>
             <div className="flex flex-rows">
-              <div className="flex flex-col mt-4 text-xl gap-9 self-start">
+              <div className="flex flex-col mt-4 lg:text-xl text-sm gap-9 lg:self-start lg:ml-0 ml-20">
                 <div>Full Name</div>
                 <div>Education Level</div>
                 <div>Hobbies</div>
                 <div>Family Size</div>
                 <div>Gender</div>
                 <div>Birth Date</div>
-                <div>Email</div>
+                <div className='mt-2'>Email</div>
                 <div>Phone Number</div>
-                <div>Upload ID</div>
+                <div className='mt-2'>Upload ID</div>
               </div>
-              <div className="flex flex-col gap-7 mt-1 ml-6 ">
+              <div className="flex flex-col gap-7 lg:mt-1 mt-3 ml-6 ">
                 <div>
                   <input
-                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 w-[28.5em]"
+                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 lg:w-[28.5em] w-[16em]"
                     id="fullname"
                     name="fullname"
                     type="text"
@@ -40,7 +43,7 @@ function EditProfileMain() {
                 </div>
                 <div>
                   <div className="relative w-full lg:max-w-sm">
-                    <select className="w-[25em] p-2 ml-6 text-gray-500 bg-white border border-SubTexts rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+                    <select className="lg:w-[25em] w-[16em] p-2 ml-6 text-gray-500 bg-white border border-SubTexts rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
                       <option value="" selected="selected" disabled="disabled">
                         -- Select Education --
                       </option>
@@ -71,7 +74,7 @@ function EditProfileMain() {
                 </div>
                 <div>
                   <input
-                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 w-[28.5em]"
+                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 lg:w-[28.5em] w-[16em]"
                     id="hobby"
                     name="hobby"
                     type="text"
@@ -87,7 +90,7 @@ function EditProfileMain() {
                   <div className="self-center ml-4">Member(s)</div>
                 </div>
                 <div>
-                  <select className="w-[25em] p-2 ml-6 text-gray-500 bg-white border border-SubTexts rounded-lg shadow-sm outline-none appearance-none focus:border-SubTexts">
+                  <select className="lg:w-[25em] w-[16em] p-2 ml-6 text-gray-500 bg-white border border-SubTexts rounded-lg shadow-sm outline-none appearance-none focus:border-SubTexts">
                     <option value="" selected="selected" disabled="disabled">
                       -- Select Gender --
                     </option>
@@ -97,21 +100,21 @@ function EditProfileMain() {
                 </div>
                 <div className="flex flex-row">
                   <input
-                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 w-[5em]"
+                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 lg:w-[5em] w-[3em]"
                     id="birthmonth"
                     name="birthmonth"
                     type="text"
                     placeholder="MM"
                   />
                   <input
-                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-2 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 w-[5em]"
+                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-2 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 lg:w-[5em] w-[3em]"
                     id="birthday"
                     name="birthday"
                     type="text"
                     placeholder="DD"
                   />
                   <input
-                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-4 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 w-[17em]"
+                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-4 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 lg:w-[17em] w-[8.5em]"
                     id="birthyear"
                     name="birthyear"
                     type="text"
@@ -120,7 +123,7 @@ function EditProfileMain() {
                 </div>
                 <div>
                   <input
-                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 w-[28.5em]"
+                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 lg:w-[28.5em] w-[16em]"
                     id="email"
                     name="email"
                     type="email"
@@ -128,51 +131,54 @@ function EditProfileMain() {
                 </div>
                 <div>
                   <input
-                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 w-[28.5em]"
+                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 lg:w-[28.5em] w-[16em]"
                     id="phone"
                     name="phone"
                     type="text"
                   />
                 </div>
-                <div>
-                  <input
-                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 w-[28.5em]"
+                <div className='flex flex-row'>
+                  <input 
+                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 lg:w-[28.5em] w-[16em]"
                     id="uploadID"
                     name="uploadID"
                     type="text"
                   />
+                  <img src={plusIcon} alt="plusIcon" className='w-6 ml-[-2em]'/>
                 </div>
               </div>
             </div>
           </div>
-          <div className="ml-20">
-            <div className="text-5xl mt-12">Security</div>
+          <div className="lg:ml-20 ml-[-10em]">
+            <div className="lg:text-5xl text-2xl mt-12">Security</div>
             <div className="flex flex-rows mt-6">
-              <div className="flex flex-col mt-4 text-xl gap-9 self-start">
+              <div className="flex flex-col mt-4 lg:text-xl text-base gap-9 self-start">
                 <div>Password</div>
-                <div>Confirm Password</div>
+                <div className='lg:mt-0 mt-[-1em]'>Confirm Password</div>
               </div>
-              <div className="flex flex-col gap-7 mt-1">
-                <div>
+              <div className="flex flex-col gap-7 mt-1 lg:ml-0 ml-4">
+                <div className='flex flex-row'>
                   <input
-                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 w-[28em]"
+                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block lg:p-2 p-1 lg:w-[28em] w-[17.5em]"
                     id="password"
                     name="password"
                     type="text"
                   />
+                  <img src={passwordIcon} alt="passwordIcon" className="w-6 ml-[-2em]"/>
                 </div>
-                <div>
+                <div className='flex flex-row'>
                   <input
-                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block  lg:p-2 p-1 w-[28em]"
+                    className="bg-gray-50 border border-SubTexts text-gray-900 sm:text-sm rounded-lg ml-6 focus:ring-primary-600 focus:border-primary-600 block  lg:p-2 p-1 lg:w-[28em] w-[17.5em]"
                     id="confirmpassword"
                     name="confirmpassword"
                     type="text"
                   />
+                  <img src={passwordIcon} alt="passwordIcon" className="w-6 ml-[-2em]"/>
                 </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-rows gap-8 mt-10 ml-20">
+          <div className="flex flex-rows lg:gap-8 gap-3 mt-10 lg:ml-20 ml-[-12em] lg:text-base text-sm">
             <button
               type="button"
               className="rounded-md box-border p-2 pl-6 pr-6 transition-all duration-250 bg-Buttons hover:bg-cyan-500 "
@@ -187,28 +193,28 @@ function EditProfileMain() {
             </button>
             <button
               type="button"
-              className="rounded-md box-border p-2 pl-16 pr-16   transition-all duration-250 bg-Buttons hover:bg-cyan-500 "
+              className="rounded-md box-border p-2 lg:pl-16 lg:pr-16  pl-8 pr-8 transition-all duration-250 bg-Buttons hover:bg-cyan-500 "
             >
               CANCEL
             </button>
           </div>
-          <div className="flex flex-col mt-4 ml-20 mt-16">
-            <div className="text-5xl">Payment Methods & Tickets</div>
+          <div className="flex flex-col mt-4 lg:ml-20 ml-[-10em] mt-16">
+            <div className="lg:text-5xl text-2xl">Payment Methods & Tickets</div>
             <div className="flex flex-rows gap-10 mt-8 mb-16">
               <div className="flex flex-col">
-                <div className="text-lg mb-2">3 Cards Added</div>
+                <div className="lg:text-lg text-sm mb-2">3 Cards Added</div>
                 <button
                   type="button"
-                  className="rounded-md box-border p-2 pl-8 pr-8 transition-all duration-250 bg-Buttons hover:bg-cyan-500 "
+                  className="rounded-md lg:text-base text-sm box-border p-2 pl-8 pr-8 transition-all duration-250 bg-Buttons hover:bg-cyan-500 "
                 >
                   SHOW CARDS
                 </button>
               </div>
               <div className="flex flex-col">
-                <div className="text-lg mb-2">10 Tickets Remaining</div>
+                <div className="lg:text-lg text-sm mb-2 ">10 Tickets Remaining</div>
                 <button
                   type="button"
-                  className="rounded-md box-border p-2 pl-10 pr-10 transition-all duration-250 bg-Buttons hover:bg-cyan-500 "
+                  className="rounded-md lg:text-base text-sm box-border p-2 pl-10 pr-10 transition-all duration-250 bg-Buttons hover:bg-cyan-500 "
                 >
                   BUY TICKETS
                 </button>
