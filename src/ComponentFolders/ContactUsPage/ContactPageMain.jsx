76449d464726c124c6bbd5ba91b8bc629162b7d7
selvidugan <selvidugan@gmail.com>
2023-01-03 21:58:53 +0300

    style: basic design

---
+++ src/ComponentFolders/ContactUsPage/ContactPageMain.jsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import OfficeImage from './Images/OfficeImage.svg'
+
+const ContactPageMain = () => {
+  return (
+    <div className='flex lg:flex-row flex-col w-screen h-screen font-poppins'>
+        <div className='basis-3/5 lg:ml-28 ml-10 lg:pr-36 md:pr-20 pr-10'>
+            <div className='lg:text-5xl md:text-3xl text-2xl leading-6 lg:mt-20 mt-14'>
+                <h1>SEND US YOUR REQUEST!</h1>
+                <div className='w-full lg:text-xl text-lg leading-8 mt-4 lg:ml-0 ml-4 lg:mr-0 text-SubTexts'>
+                <h3>
+                Do you have a question, concern, idea, feedback, or problem?
+                If you need assistance, please fill out the form below
+                and we would be happy to help!
+                </h3>
+                </div>
+            </div>
+        </div>
+        <div className='form check'>
+            <div className='mt-2'>
+                <h3 className='lg:text-xl text-lg lg:mt-10 mt-8 lg:ml-0 ml-4 lg:mr-0 font-semibold'>
+                    Type of contact
+                </h3>
+                <ul className="w-48 text-sm font-medium mt-4 lg:text-xl lg:mt-5 lg:ml-2 ml-4 lg:mr-0">
+                    <li className="w-full">
+                            <label 
+                            htmlFor="list-radio-1" 
+                            className="w-full py-3 text-sm font-medium">
+                                <input 
+                                id="list-radio-1" 
+                                type="radio" 
+                                name="list-radio" 
+                                className="w-4 h-4 focus:ring-blue-500"/>
+                                    <span className='ml-2'>
+                                        I have a question about the service.
+                                    </span> 
+                            </label>
+                    </li>
+                    <li className="w-full">
+                            <label 
+                            htmlFor="list-radio-1" 
+                            className="w-full py-3 text-sm font-medium">
+                                <input 
+                                id="list-radio-1" 
+                                type="radio" 
+                                name="list-radio" 
+                                className="w-4 h-4 focus:ring-blue-500"/>
+                                    <span className='ml-2'>
+                                    I am a registered client and I need support.
+                                    </span> 
+                            </label>
+                    </li>
+                    <li className="w-full">
+                            <label 
+                            htmlFor="list-radio-1" 
+                            className="w-full py-3 text-sm font-medium">
+                                <input 
+                                id="list-radio-1" 
+                                type="radio" 
+                                name="list-radio" 
+                                className="w-4 h-4 focus:ring-blue-500"/>
+                                    <span className='ml-2'>
+                                        I am a counselor interested in joining.
+                                    </span> 
+                            </label>
+                    </li>
+                    <li className="w-full">
+                            <label 
+                            htmlFor="list-radio-1" 
+                            className="w-full py-3 text-sm font-medium">
+                                <input 
+                                id="list-radio-1" 
+                                type="radio" 
+                                name="list-radio" 
+                                className="w-4 h-4 focus:ring-blue-500"/>
+                                    <span className='ml-2'>
+                                    I am a registered counselor and I need support.
+                                    </span> 
+                            </label>
+                    </li>
+                    <li className="w-full">
+                            <label 
+                            htmlFor="list-radio-1" 
+                            className="w-full py-3 text-sm font-medium">
+                                <input 
+                                id="list-radio-1" 
+                                type="radio" 
+                                name="list-radio" 
+                                className="w-4 h-4 focus:ring-blue-500"/>
+                                    <span className='ml-2'>
+                                    I have a business-related inquiry.
+                                    </span> 
+                            </label>
+                    </li>
+                    <li className="w-full">
+                            <label 
+                            htmlFor="list-radio-1" 
+                            className="w-full py-3 text-sm font-medium">
+                                <input 
+                                id="list-radio-1" 
+                                type="radio" 
+                                name="list-radio" 
+                                className="w-4 h-4 focus:ring-blue-500"/>
+                                    <span className='ml-2'>
+                                    I am interested in Healing Online for my organization.
+                                    </span> 
+                            </label>
+                    </li>
+                    <li className="w-full">
+                            <label 
+                            htmlFor="list-radio-1" 
+                            className="w-full py-3 text-sm font-medium">
+                                <input 
+                                id="list-radio-1" 
+                                type="radio" 
+                                name="list-radio" 
+                                className="w-4 h-4 focus:ring-blue-500"/>
+                                    <span className='ml-2'>
+                                    I have a billing related question.
+                                    </span> 
+                            </label>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+        
+        <div className='basis-2/5 lg:mt-32 lg:pr-16 lg:ml-0 md:ml-40 ml-10 mt-12 mr-10'>
+        <img src={OfficeImage} alt="OfficeImg" className='mb-10'/>
+      </div>
+    </div>
+        
+    
+  )
+}
+
+export default ContactPageMain
