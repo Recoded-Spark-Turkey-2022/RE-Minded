eb983802a4f4952c59cc3a56f9e9f687cf438ad0
Menar Selamet <menarselamet007@gmail.com>
2023-01-07 13:54:32 +0300

    Merged Booking Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-//  import {  Routes, Route } from 'react-router-dom';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import About from './ComponentFolders/AboutPage/About';
