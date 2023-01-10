e86b710113ea70c00e9cfd09fd0af5be6fd6c3df
SeymaDemir <seymademir35@gmail.com>
2023-01-10 18:54:48 +0300

    not yet finish

--- src/ComponentFolders/TherapistCreatePage/TherapistCreatePage.jsx
+++ src/ComponentFolders/TherapistCreatePage/TherapistCreatePage.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 
-// const thanksProps="Thank you for your interest in working with Healing, we have recieved your application.You will receive an email guiding you for the next steps soon after your information is reviewed."
+const thanksProps="Thank you for your interest in working with Healing, we have recieved your application.You will receive an email guiding you for the next steps soon after your information is reviewed."
 
 function TherapistCreatePage() {
 
+  const navigate = useNavigate();
+
   const {
-    register,
-    handleSubmit,
+    register,getValues,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
 
   return (
-    <div className=" font-poppins h-screen flex flex-col justify-center content-center lg:ml-56 lg:mr-64 ml-10 mr-10 sm:ml-16 sm:mr-16 md:ml-32 md:mr-32 lg:mt-10 mt-20">
+    <div className=" font-poppins flex flex-col justify-center content-center lg:ml-56 lg:mr-64 ml-10 mr-10 sm:ml-16 sm:mr-16 md:ml-32 md:mr-32 lg:mt-10 mt-20">
       <h1 className="lg:mt-16 mt-4 lg:text-5xl text-xl">CREATE AN ACCOUNT</h1>
 
-      <form className="mb-0 mt-4 space-y-6" onSubmit={handleSubmit(onSubmit)}>
+      <form className="mb-0 mt-4 space-y-6" onSubmit={() =>  navigate('/thankyou', { replace: true, state: thanksProps })}>
         <div>
           <label htmlFor="username">
             <span className="lg:text-xl text-sm">User Name</span>
@@ -95,6 +93,7 @@ function TherapistCreatePage() {
           <label htmlFor="password">
             <span className="lg:text-xl text-sm">Create Password</span>
             <input
+            
              {...register("password", {
               required: true,
               minLength: 6
@@ -119,28 +118,27 @@ function TherapistCreatePage() {
           <label htmlFor="confirm">
             <span className="lg:text-xl text-sm">Confirm Password</span>
             <input
-             {...register("password", {
+             {...register("confirm", {
+              checkPsd: value => value === getValues().password,
               required: true,
-              minLength: 6
             })}
               id="confirm"
               name="confirm"
-              type="confirm-password"
+              type="password"
               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1 "
             />
           </label>
-          {errors.password && errors.password.type === "required" && (
-            <p className="errorMsg text-red-400 lg:text-base text-sm">Password is required.</p>
+          {errors.confirm && errors.confirm.type === "required" && (
+            <p className="errorMsg text-red-400 lg:text-base text-sm">Confirm password</p>
           )}
-          {errors.password && errors.password.type === "minLength" && (
+          {errors.confirm && errors.confirm.type === "checkPsd" && (
             <p className="errorMsg text-red-400 lg:text-base text-sm">
-              Password should be at-least 6 characters.
+              Password confirm does not match
             </p>
-          )}
+          )} 
         </div>
 
         <div>
-      
           <button
             type="submit"
             className="rounded-md box-border p-2 lg:pl-10 lg:pr-10 pl-4 pr-4 mb-12 font-bold lg:text-xl text-sm text-gray-800 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 "
