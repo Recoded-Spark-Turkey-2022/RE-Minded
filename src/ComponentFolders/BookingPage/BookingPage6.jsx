057c3664fcf6656005624a76e54202a820c4cd09
Menar Selamet <menarselamet007@gmail.com>
2023-01-10 12:27:37 +0300

    Build: page 4 page 5 buttons activated

--- src/ComponentFolders/BookingPage/BookingPage6.jsx
+++ src/ComponentFolders/BookingPage/BookingPage6.jsx
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom';
 import Card1 from './Card';
 
 function BookingPage6() {
+
+
   return (
     <div>
       <div className="flex flex-col items-center p-12">
@@ -11,9 +13,12 @@ function BookingPage6() {
           content="Please specify (in a few sentences) why you'd like counseling.This will give your counselor a good understanding of where to start."
         />
 
-        <textarea className="font-poppins pb-72 p-8 shadow-zinc-300  box-border h-600 w-4/5 md:max-w-2xl lg:max-w-4xl  shadow-md" />
+        <textarea
+          required
+          className="font-poppins pb-72 p-8 shadow-zinc-300  box-border h-600 w-4/5 md:max-w-2xl lg:max-w-4xl  shadow-md"
+        />
       </div>
-      <div className="flex justify-center md:justify-evenly lg:justify-evenly ">
+      <div className="flex justify-center md:justify-evenly lg:justify-evenly pb-24 ">
         <Link to="/booking5">
           <button
             type="button"
