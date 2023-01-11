d4d25f05dff95e4cb6f9663aa1d10e8ccd8fb437
Menar Selamet <menarselamet007@gmail.com>
2023-01-11 21:29:59 +0300

    minor

--- src/ComponentFolders/BookingPage/QuestionChoicesA.jsx
+++ src/ComponentFolders/BookingPage/QuestionChoicesA.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import { React, useState} from 'react';
 
 function ChoicesA({ question, first, second, third, showThird }) {
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(() => !isClicked);
+  }
+
+
   return (
     <div className="p-10">
       <h1 className="text-xl pb-6 md:text-2xl lg:text-3xl lg:flex justify-center mt-4">
         {question}
       </h1>
-      <div className="flex flex-col pl-6 text-md md:text-lg lg:text-xl">
-        <div className="shadow-zinc-300 flex flex-col w-full mb-6 mt-12 px-4 py-4 shadow-md hover:bg-cyan-400 cursor-pointer">
+      <div
+        className="flex flex-col pl-6 text-md md:text-lg lg:text-xl"
+      >
+        <button
+          id={1}
+          type="button"
+          onClick={handleClick}
+          className={`shadow-zinc-300 flex flex-col w-full mb-6 mt-12 px-4 py-4 shadow-md hover:bg-cyan-400 ${
+            isClicked ? 'bg-red-800' : ''
+          }`}
+        >
           {first}
-        </div>
-        <div className="shadow-zinc-300 flex flex-col w-full mb-6 px-4 py-4 shadow-md hover:bg-cyan-400 cursor-pointer">
+        </button>
+        <button
+          id={1}
+          type="button"
+          onClick={handleClick}
+          className="shadow-zinc-300 flex flex-col w-full mb-6 px-4 py-4 shadow-md hover:bg-cyan-400 "
+        >
           {second}
-        </div>
+        </button>
         {showThird && (
-          <div className="shadow-zinc-300 flex flex-col w-full mb-6 px-4 py-4 shadow-md hover:bg-cyan-400 cursor-pointer">
+          <button
+            id={1}
+            type="button"
+            className="shadow-zinc-300 flex flex-col w-full mb-6 px-4 py-4 shadow-md hover:bg-cyan-400 "
+          >
             {third}
-          </div>
+          </button>
         )}
       </div>
     </div>
--- src/ComponentFolders/TherapistCreatePage/TherapistCreatePage.jsx
+++ src/ComponentFolders/TherapistCreatePage/TherapistCreatePage.jsx
@@ -1,86 +1,149 @@
 import React from 'react';
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+
+const thanksProps =
+  'Thank you for your interest in working with Healing, we have recieved your application.You will receive an email guiding you for the next steps soon after your information is reviewed.';
 
 function TherapistCreatePage() {
-  return (
-    <div className=" font-poppins h-screen flex flex-col justify-center content-center lg:ml-56 lg:mr-64 ml-10 mr-10 sm:ml-16 sm:mr-16 md:ml-32 md:mr-32 lg:mt-10 mt-20">
-      <h1 className="lg:mt-16 mt-4 lg:text-5xl text-xl">CREATE AN ACCOUNT</h1>
+  const navigate = useNavigate();
 
-      <form className="mb-0 mt-4 space-y-6" action="#" method="POST">
-        <div>
-          <label htmlFor="username">
+  const yupValidation = Yup.object().shape({
+    username: Yup.string()
+      .required('Please enter username.')
+      .min(6, 'Add minimum 6 characters'),
+    email: Yup.string().required('Email is required').email(),
+    city: Yup.string().required('Please enter a city.'),
+    license: Yup.string().required('Please enter a license number.'),
+    password: Yup.string()
+      .required('Please enter a password.')
+      .min(6, 'Password must be at least 6 characters'),
+    confirm: Yup.string()
+      .required('Please confirm password.')
+      .oneOf([Yup.ref('password'), null], 'Passwords does not match'),
+  });
+  const formOptions = { resolver: yupResolver(yupValidation) };
+  const { register, handleSubmit, formState } = useForm(formOptions);
+  const { errors } = formState;
+  function onSubmit(data) {
+    console.log(JSON.stringify(data, null, 4));
+    navigate('/thankyou', { replace: true, state: thanksProps });
+    return false;
+  }
+  return (
+    <div className=" font-poppins flex flex-col justify-center content-center lg:ml-56 lg:mr-64 ml-10 mr-10 sm:ml-16 sm:mr-16 md:ml-32 md:mr-32 lg:mt-10 mt-20">
+      <h1 className="lg:mt-16 mt-4 lg:text-5xl text-xl">CREATE AN ACCOUNT</h1>
+      <form className="mb-0 mt-4 space-y-6" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label>
             <span className="lg:text-xl text-sm">User Name</span>
             <input
-              id="username"
               name="username"
               type="text"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1 "
+              className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1  ${
+                errors.username ? 'is-invalid' : ''
+              }`}
+              {...register('username')}
             />
           </label>
+          <div className="invalid-feedback text-red-400 lg:text-base text-sm">
+            {errors.username?.message}
+          </div>
         </div>
 
-        <div>
+        <div className="form-group">
           <label htmlFor="email">
             <span className="lg:text-xl text-sm">Email</span>
             <input
-              id="email"
               name="email"
-              type="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1 "
+              type="text"
+              className={`form-control bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1 ${
+                errors.email ? 'is-invalid' : ''
+              }`}
+              {...register('email')}
             />
           </label>
+          <div className="invalid-feedback text-red-400 lg:text-base text-sm">
+            {errors.email?.message}
+          </div>
         </div>
 
-        <div>
-          <label htmlFor="city">
+        <div className="form-group">
+          <label>
             <span className="lg:text-xl text-sm">City</span>
             <input
-              id="city"
               name="city"
               type="text"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1 "
+              className={`form-control bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1 ${
+                errors.city ? 'is-invalid' : ''
+              }`}
+              {...register('city')}
             />
           </label>
+
+          <div className="invalid-feedback text-red-400 lg:text-base text-sm">
+            {errors.city?.message}
+          </div>
         </div>
 
-        <div>
-          <label htmlFor="licensenumber">
+        <div className="form-group">
+          <label>
             <span className="lg:text-xl text-sm">License Number</span>
             <input
-              id="licensenumber"
-              name="licensenumber"
-              type="text" // number?
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1"
+              name="license"
+              type="text"
+              className={`form-control bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1 ${
+                errors.license ? 'is-invalid' : ''
+              }`}
+              {...register('license')}
             />
           </label>
+          <div className="invalid-feedback text-red-400 lg:text-base text-sm">
+            {errors.license?.message}
+          </div>
         </div>
 
-        <div>
-          <label htmlFor="password">
-            <span className="lg:text-xl text-sm">Create Password</span>
+        <div className="form-group">
+          <label>
+            <span className="lg:text-xl text-sm">Password</span>
             <input
-              id="password"
               name="password"
               type="password"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1"
+              className={`form-control bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1 ${
+                errors.password ? 'is-invalid' : ''
+              }`}
+              {...register('password')}
             />
           </label>
+
+          <div className="invalid-feedback text-red-400 lg:text-base text-sm">
+            {errors.password?.message}
+          </div>
         </div>
 
-        <div>
-          <label htmlFor="confirm">
+        <div className="form-group">
+          <label>
             <span className="lg:text-xl text-sm">Confirm Password</span>
             <input
-              id="confirm"
               name="confirm"
-              type="confirm-password"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1 "
+              type="password"
+              className={`form-control bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full lg:p-2.5 p-1 ${
+                errors.confirm ? 'is-invalid' : ''
+              }`}
+              {...register('confirm')}
             />
           </label>
+
+          <div className="invalid-feedback text-red-400 lg:text-base text-sm">
+            {errors.confirm?.message}
+          </div>
         </div>
 
-        <div>
+        <div className="mt-3">
           <button
-            type="button"
+            type="submit"
             className="rounded-md box-border p-2 lg:pl-10 lg:pr-10 pl-4 pr-4 mb-12 font-bold lg:text-xl text-sm text-gray-800 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 "
           >
             CREATE
