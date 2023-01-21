e30e6cfb0a570faa9cab03a3e7b0d3b2cb78243d
SeymaDemir <seymademir35@gmail.com>
2023-01-21 11:58:14 +0300

    added external links for blogs

--- src/ComponentFolders/BlogPage/RecommendedforYou.jsx
+++ src/ComponentFolders/BlogPage/RecommendedforYou.jsx
@@ -9,8 +9,8 @@ function RecommendedforYou() {
             <div className=" mt-20">
                 <h2 className="font-poppins uppercase lg:ml-48 ml-10 md:text-2xl text-xl">Recommended for you</h2>
                 <div className=" flex flex-col lg:flex-row mt-6 mb-20 justify-center gap-10 ml-0 lg:ml-[-10em]">
-                        <img src={Image1} alt="Heal" className="self-start lg:ml-0 ml-10" />
-                        <img src={Image2} alt="Need" className="self-start lg:ml-0 ml-10"  />
+                        <a target="_blank" rel="noreferrer" href="https://www.verywellmind.com/10-ways-to-heal-from-trauma-5206940" ><img src={Image1} alt="Heal" className="self-start lg:ml-0 ml-10" /></a>
+                        <a target="_blank" rel="noreferrer" href="https://www.healthline.com/health/anxiety"><img src={Image2} alt="Need" className="self-start lg:ml-0 ml-10"  /></a>
                 </div>
             </div>
         </div>
