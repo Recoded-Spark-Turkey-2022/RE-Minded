97ab280c98d2e2cbc1640ad08b766cf3a7361e1a
selvidugan <selvidugan@gmail.com>
2023-01-21 13:23:22 +0300

    translation:login page done

--- src/ComponentFolders/LoginPage/Login.jsx
+++ src/ComponentFolders/LoginPage/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
 import { useSelector } from 'react-redux';
 import { signInWithEmailAndPassword } from 'firebase/auth';
@@ -42,12 +43,13 @@ function Login() {
       };
     });
   }
+  const { t } = useTranslation();
 
   return (
     <div className="h-screen flex justify-center content-center md:flex-wrap max-[767px]:flex-wrap gap-x-20">
       <div>
         <h2 className='text-5xl font-["Poppins"] font-normal mb-44 max-[767px]:mt-20 md:mt-20 max-[767px]:mb-10 md:mb-10'>
-          LOGIN
+          {t('login.h1')}
         </h2>
         <form
           onSubmit={login}
@@ -55,7 +57,7 @@ function Login() {
         >
           <input
             type="text"
-            placeholder="   Your Email"
+            placeholder=   {t('login.email')}
             name="userEmail"
             onChange={(e) => handleOnClick(e)}
             className="h-20 broder-solid border-2 border-[#D1DBE3] rounded-md focus:outline-none focus:placeholder-white"
@@ -63,7 +65,7 @@ function Login() {
           />
           <input
             type="password"
-            placeholder="   Your Password"
+            placeholder=   {t('login.password')}
             name="userPassword"
             onChange={(e) => handleOnClick(e)}
             className="h-20 broder-solid border-2 border-[#D1DBE3] rounded-md focus:outline-none focus:placeholder-white"
@@ -74,20 +76,20 @@ function Login() {
               type="submit"
               className="bg-[#2DD3E3] font-medium text-2xl px-14 rounded-md shadow-[0px_7px_20px_rgba(0,0,0,0.2)]"
             >
-              Login
+              {t('login.login')}
             </button>
             <button
               type="button"
               className="broder-solid border-2 border-[#2DD3E3] font-medium text-2xl px-14 rounded-md"
               onClick={() => navigate('/signup')}
             >
-              Signup
+              {t('login.signup')}
             </button>
           </div>
         </form>
         <div className="flex justify-around my-6">
           <img src={lineImage} alt="A line" />
-          <p>Or</p>
+          <p>{t('login.or')}</p>
           <img src={lineImage} alt="A line" />
         </div>
         <div className="flex justify-center my-6 gap-x-20">
