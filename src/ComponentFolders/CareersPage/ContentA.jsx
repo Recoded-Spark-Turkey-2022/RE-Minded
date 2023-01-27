51365d031cf171b12e2a4f73b9a82424c6b22a07
selvidugan <selvidugan@gmail.com>
2023-01-27 10:55:29 +0300

    careers, contactus, editprofile, footer

--- src/ComponentFolders/CareersPage/ContentA.jsx
+++ src/ComponentFolders/CareersPage/ContentA.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
+import { useTranslation } from 'react-i18next';
 
 function ContentA() {
+
+  const { t } = useTranslation();
+
   return (
     <div>
       <div className="lg:text-5xl md:text-3xl text-2xl leading-6 mt-10 lg:ml-28 ml-10 text-gray-700">
-        <h1>CAREERS AT HEALING</h1>
+        <h1>{t('careersa.h1')}</h1>
       </div>
       <div className="lg:text-xl text-lg leading-6 mt-4 lg:ml-28 ml-10 text-gray-400">
-        <h3>Be a part of making people feel better.</h3>
+        <h3>{t('careersa.h2')}</h3>
       </div>
       <div className="lg:text-lg text-base leading-6 lg:mt-24 mt-16 lg:ml-28 ml-10 lg:mr-52 md:mr-20 mr-10 text-gray-700">
-        Our SEO software cuts through mountains of data to surface critical
-        insights. We build and maintain systems that process massive amounts of
-        data (we&apos;re talking 36 trillion records per day and multiple
-        petabytes of storage.) We model transparent and empathetic marketing for
-        the world. We educate our community, making every effort to help them
-        improve their skill. And we do it all by fostering a culture that
-        encourages accountability, empathy, and transparency.
+      {t('careersa.t1')}
       </div>
       <div className="lg:text-2xl text-lg leading-6 mt-20 lg:ml-28 ml-10 text-gray-700">
-        <h3>What role will you play?</h3>
+        <h3>{t('careersa.h3')}</h3>
       </div>
       <div className="text-xl leading-6 mt-4 lg:ml-12 mb-8">
       <Link to="/careers#openpositions">
@@ -28,7 +26,7 @@ function ContentA() {
           type="button"
           className="lg:text-xl md:text-base text-sm lg:ml-16 ml-10 lg:px-6 rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 cursor-pointer "
         >
-          SEE OUR OPEN LIST
+          {t('careersa.btn1')}
         </button></Link>
       </div>
     </div>
