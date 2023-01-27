4c83224cffbaa035a5705836fd47c4070ebf87b9
selvidugan <selvidugan@gmail.com>
2023-01-27 10:47:37 +0300

    blog pages, booking pages

--- src/ComponentFolders/BookingPage/BookingPage5.jsx
+++ src/ComponentFolders/BookingPage/BookingPage5.jsx
@@ -1,5 +1,6 @@
 import { React, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import Card1 from './Card';
 // import ChoicesB from './QuestionChoicesB';
 
@@ -26,20 +27,21 @@ function BookingPage5() {
     if (Object.values(checkboxes).some((val) => val === true)) {
       navigate('/booking6');
     } else {
-      // eslint-disable-next-line no-alert
       alert('Please select at least one!');
     }
   }
+  
+  const { t } = useTranslation();
 
   return (
     <div className="flex flex-col items-center p-12">
       <Card1
-        header="LET'S MATCH YOU WITH THE RIGHT THERAPIST"
-        content="Please fill out this short questionnaire to provide some general and anonymous background about you and the issues you'd like to deal with in online therapy. It would help us match you with the most suitable therapist for you. "
+        header={t('booking.header')}
+        content={t('booking.content')}
       />
       <div className="font-poppins shadow-zinc-300 flex flex-col w-full md:max-w-2xl lg:max-w-4xl my-16 mx-auto px-8 py-4 shadow-md">
         <h1 className="text-xl pb-6 md:text-2xl lg:text-3xl lg:flex justify-center mt-4">
-          Are there any issues you would like to focus on?
+        {t('booking5.h1')}
         </h1>
         <form className="flex flex-col pl-6 text-md md:text-lg  lg:text-xl">
           <label htmlFor="one">
@@ -50,7 +52,7 @@ function BookingPage5() {
               checked={checkboxes.option1}
               onChange={handleCheckboxChange}
             />
-            Depression
+            {t('booking5.b1')}
           </label>
           <label htmlFor="two">
             <input
@@ -60,7 +62,7 @@ function BookingPage5() {
               checked={checkboxes.option2}
               onChange={handleCheckboxChange}
             />
-            Stress and Anxiety
+            {t('booking5.b2')}
           </label>
           <label htmlFor="three">
             <input
@@ -70,7 +72,7 @@ function BookingPage5() {
               checked={checkboxes.option3}
               onChange={handleCheckboxChange}
             />
-            Relationship issues
+            {t('booking5.b3')}
           </label>
           <label htmlFor="four">
             <input
@@ -80,7 +82,7 @@ function BookingPage5() {
               checked={checkboxes.option4}
               onChange={handleCheckboxChange}
             />
-            Family conflicts
+            {t('booking5.b4')}
           </label>
           <label htmlFor="five">
             <input
@@ -90,7 +92,7 @@ function BookingPage5() {
               checked={checkboxes.option5}
               onChange={handleCheckboxChange}
             />
-            Trauma and abuse
+            {t('booking5.b5')}
           </label>
           <label htmlFor="six">
             <input
@@ -100,7 +102,7 @@ function BookingPage5() {
               checked={checkboxes.option6}
               onChange={handleCheckboxChange}
             />
-            Eating disorders
+            {t('booking5.b6')}
           </label>
         </form>
         <div className="flex justify-center md:justify-between lg:justify-between ">
@@ -109,7 +111,7 @@ function BookingPage5() {
               type="button"
               className="m-6 rounded-md box-border text-xl pr-6 pl-6 pt-2 pb-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 lg:text-2xl"
             >
-              PREVIOUS
+              {t('booking.previous')}
             </button>
           </Link>
           <button
@@ -117,7 +119,7 @@ function BookingPage5() {
             onClick={handleClick}
             className="m-6 rounded-md box-border text-xl pr-6 pl-6 pt-2 pb-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 lg:text-2xl"
           >
-            NEXT
+            {t('booking.next')}
           </button>
         </div>
       </div>
--- src/ComponentFolders/BookingPage/BookingPage8.jsx
+++ src/ComponentFolders/BookingPage/BookingPage8.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import Card from './Card';
 
 function BookingPage8() {
+
+  const { t } = useTranslation();
+
   return (
     <div className="flex flex-col  p-24">
       <Card
-        header="YOUR REQUEST HAS BEEN SUBMITTED!"
-        content="You will receive an email guiding you to book a date and time soon."
+        header={t('booking8.header')}
+        content={t('booking8.content')}
       />
       <div className="font-poppins shadow-zinc-300 flex flex-col items-center my-16 mx-auto px-16 py-24 shadow-md">
         <h1 className="text-xl pb-12 md:text-2xl lg:text-3xl lg:flex justify-center">
-          Request Submitted
+        {t('booking8.h1')}
         </h1>
         <h2 className="text-md pb-10 md:text-lg lg:text-xl lg:flex justify-center">
-          You Will Receive A Confirmation Email Soon Please Keep An Eye On Your
-          Mail.
+        {t('booking8.h2')}
         </h2>
         <div className="flex justify-center">
           <Link to="/">
@@ -23,7 +26,7 @@ function BookingPage8() {
               type="button"
               className="m-6 rounded-md box-border text-md md:text-lg  pr-6 pl-6 pt-2 pb-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 lg:text-2xl"
             >
-              BACK TO HOME
+              {t('booking8.btn')}
             </button>
           </Link>
         </div>
