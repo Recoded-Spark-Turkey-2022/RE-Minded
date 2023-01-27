5ea301502cb179ac8d300a4390e44f61483fe435
selvidugan <selvidugan@gmail.com>
2023-01-27 11:11:33 +0300

    home pages, login page, navbar pages and navbar lang dropdown

--- src/ComponentFolders/LoginPage/Login.jsx
+++ src/ComponentFolders/LoginPage/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
+import { useSelector } from 'react-redux';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import Image from './Images/LoginSofa.svg';
 import lineImage from './Images/line.svg';
@@ -9,29 +11,27 @@ import { signInWithGoogle, signInWithFacebook, auth } from '../../Firebase';
 
 function Login() {
   const navigate = useNavigate();
-
+  const currentUser = useSelector((state) => state.currentUser);
   const [loginData, setLoginData] = useState({
     userEmail: '',
     userPassword: '',
   });
 
-  const login = (e) => {
-    e.preventDefault();
-
-    signInWithEmailAndPassword(
-      auth,
-      loginData.userEmail,
-      loginData.userPassword
-    )
-      .then((user) => {
-        if (user) {
-          navigate('/');
-        }
-      })
-      .catch(() =>
-        // eslint-disable-next-line no-alert
-        alert('Try another email and password')
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginData.userEmail,
+        loginData.userPassword
       );
+      return user;
+    } catch (error) {
+      return error;
+    } finally {
+      if (currentUser) {
+        navigate('/');
+      }
+    }
   };
 
   function handleOnClick(e) {
@@ -43,12 +43,13 @@ function Login() {
       };
     });
   }
+  const { t } = useTranslation();
 
   return (
     <div className="h-screen flex justify-center content-center md:flex-wrap max-[767px]:flex-wrap gap-x-20">
       <div>
         <h2 className='text-5xl font-["Poppins"] font-normal mb-44 max-[767px]:mt-20 md:mt-20 max-[767px]:mb-10 md:mb-10'>
-          LOGIN
+          {t('login.h1')}
         </h2>
         <form
           onSubmit={login}
@@ -56,18 +57,18 @@ function Login() {
         >
           <input
             type="text"
-            placeholder="   Your Email"
+            placeholder=   {t('login.email')}
             name="userEmail"
             onChange={(e) => handleOnClick(e)}
-            className="h-20 px-3 broder-solid border-2 border-[#D1DBE3] rounded-md focus:outline-none focus:placeholder-white"
+            className="h-20 broder-solid border-2 border-[#D1DBE3] rounded-md focus:outline-none focus:placeholder-white"
             value={loginData.userEmail}
           />
           <input
             type="password"
-            placeholder="   Your Password"
+            placeholder=   {t('login.password')}
             name="userPassword"
             onChange={(e) => handleOnClick(e)}
-            className="h-20 px-3 broder-solid border-2 border-[#D1DBE3] rounded-md focus:outline-none focus:placeholder-white"
+            className="h-20 broder-solid border-2 border-[#D1DBE3] rounded-md focus:outline-none focus:placeholder-white"
             value={loginData.userPassword}
           />
           <div className="flex justify-around py-3 gap-8">
@@ -75,27 +76,24 @@ function Login() {
               type="submit"
               className="bg-[#2DD3E3] font-medium text-2xl px-14 rounded-md shadow-[0px_7px_20px_rgba(0,0,0,0.2)]"
             >
-              Login
+              {t('login.login')}
             </button>
             <button
               type="button"
               className="broder-solid border-2 border-[#2DD3E3] font-medium text-2xl px-14 rounded-md"
               onClick={() => navigate('/signup')}
             >
-              Signup
+              {t('login.signup')}
             </button>
           </div>
         </form>
         <div className="flex justify-around my-6">
           <img src={lineImage} alt="A line" />
-          <p>Or</p>
+          <p>{t('login.or')}</p>
           <img src={lineImage} alt="A line" />
         </div>
         <div className="flex justify-center my-6 gap-x-20">
-          <button
-            type="button"
-            onClick={() => signInWithFacebook(() => navigate('/'))}
-          >
+          <button type="button" onClick={signInWithFacebook}>
             <img
               src={FacebookLogo}
               alt="Facebook logo"
@@ -103,10 +101,7 @@ function Login() {
             />
           </button>
 
-          <button
-            type="button"
-            onClick={() => signInWithGoogle(() => navigate('/'))}
-          >
+          <button type="button" onClick={signInWithGoogle}>
             <img
               src={GoogleLogo}
               alt="Google logo"
