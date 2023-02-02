2e282127956f12a25fbb19eabbfcecec8e3181aa
HomeLanderz <86560837+HomeLanderz@users.noreply.github.com>
2023-02-02 11:50:54 +0300

    Build: add firebase package

--- src/ComponentFolders/BlogPage/_test_/BlogPageMain.test.js
+++ src/ComponentFolders/BlogPage/_test_/BlogPageMain.test.js
@@ -1,7 +1,26 @@
 import React from "react";
+import firebase from "firebase";
+import firebaseMock from "firebase-mock"
 import  renderer  from "react-test-renderer";
 import BlogPageMain from "../BlogPageMain"
 
+const mockAuth = new firebaseMock.MockFirebase();
+const mockDatabase = new firebaseMock.MockFirebase();
+const mockFirestore = new firebaseMock.MockFirestore();
+
+const mockApp = firebaseMock.MockFirebaseSdk(
+  (path) => {
+    return path ? mockDatabase.child(path) : mockDatabase;
+  },
+  () => {
+    return mockAuth;
+  },
+  () => {
+    return mockFirestore;
+  }
+);
+
+firebase.initializeApp(mockApp);
 
 
 test ( "BlogPageMain test snapshot", () => { 
