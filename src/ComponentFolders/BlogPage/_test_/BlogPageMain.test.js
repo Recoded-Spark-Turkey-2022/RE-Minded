61a0a6ff619aef5842887061f3a0f6ed2a415944
HomeLanderz <86560837+HomeLanderz@users.noreply.github.com>
2023-02-02 11:41:12 +0300

    Build: add firebase-mock library

--- src/ComponentFolders/BlogPage/_test_/BlogPageMain.test.js
+++ src/ComponentFolders/BlogPage/_test_/BlogPageMain.test.js
@@ -1,11 +1,11 @@
-// import React from "react";
-// import  renderer  from "react-test-renderer";
-// import BlogPageMain from "../BlogPageMain"
+import React from "react";
+import  renderer  from "react-test-renderer";
+import BlogPageMain from "../BlogPageMain"
 
 
 
-// test ( "BlogPageMain test snapshot", () => { 
-//     const component=renderer.create(<BlogPageMain />)
-//     let tree= component.toJSON(); 
-//     expect(tree).toMatchSnapshot(); 
-// })
+test ( "BlogPageMain test snapshot", () => { 
+    const component=renderer.create(<BlogPageMain />)
+    const tree = component.toJSON(); 
+    expect(tree).toMatchSnapshot(); 
+})
